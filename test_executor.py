bdfe1d970b743e86bea3bda4ba9daa4b3acc3fcf
gordonk <oopismcgoopis@gmail.com>
2026-09-25 09:57:25 -0500

    test_executor.py:

---
+++ test_executor.py
@@ -0,0 +1,10 @@
+import asyncio
+import json
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('resources/core'))
+
+# Dummy context
+import kernel
+# wait, actually we can just use the shell!
